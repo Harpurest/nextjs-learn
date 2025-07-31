284bcdf40c7b00c32ccbdf017aba999782c73144
harpurest <vo.duc.hong94@gmail.com>
2025-07-31 17:38:24 +0700

    add CRUD & error handling

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -155,8 +155,9 @@ export async function fetchInvoiceById(id: string) {
 
     return invoice[0];
   } catch (error) {
-    console.error('Database Error:', error);
-    throw new Error('Failed to fetch invoice.');
+    // console.error('Database Error:', error);
+    // throw new Error('Failed to fetch invoice.');
+    return null;
   }
 }
 
